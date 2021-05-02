8418afd1e05f90d1ad4c347a6fb67bbea33671fa
AlenaUslontseva <auslonceva@ya.ru>
2021-05-02 17:57:38 +0300

    add commit

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 
     @RunWith(Cucumber.class)
     @CucumberOptions(
-            features = "src/test/java/feature",
-            glue = "step",
+            features = "src/resources/features",
+            glue = "step",//пакет, в котором находятся классы с реализацией шагов и «хуков».
             tags = "@test"
     )
     public class RunnerTest {
