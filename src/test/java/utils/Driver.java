1fae966a05d76a01a0120b6f9e968bd0648ec727
AlenaUslontseva <auslonceva@ya.ru>
2021-05-02 09:15:04 +0300

    add commit

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -6,6 +6,7 @@ import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +17,7 @@ public class Driver {
 
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(cfg.urlEpam());
         driver.manage().window().maximize();
